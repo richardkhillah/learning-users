004eea84d812b0ab100c423171a9e529c7b875ef
Richard Khillah <richardkhillah@Richards-MacBook-Air.local>
2022-12-11 21:19:56 -0800

    first commit

---
+++ basic_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+# For Template URLs
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+
+    path('user_login/', views.user_login, name='user-login')
+]
